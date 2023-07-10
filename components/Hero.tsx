7c1a41355c4b67b0a17109daa7b6289df467539f
Gümrah Sindar <gsindar@gmail.com>
2023-07-10 18:50:13 +0300

    add "priority" prop to handle Next.js error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,9 +25,16 @@ const Hero = () => {
       </div>
       <div className='hero__image-container'>
         <div className='hero__image'>
-          <Image src='/hero.png' alt='hero' fill className='object-contain' />
-          <div className='hero__image-overlay' />
+          <Image
+            priority
+            src='/hero.png'
+            alt='hero'
+            fill
+            className='object-contain'
+          />
         </div>
+
+        <div className='hero__image-overlay' />
       </div>
     </div>
   )
